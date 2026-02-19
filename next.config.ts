77df10067b8a499f07d9682f91997a5338f68095
Bashar ElHusseiny <basharelhusseiny@gmail.com>
2026-02-19 03:09:51 +0200

    chore: remove unused `images.remotePatterns` configuration from `next.config.ts`

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "",
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
